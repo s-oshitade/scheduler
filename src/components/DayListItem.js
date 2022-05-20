60a52f07cac07b365c6426f854d4113b087c3379
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-20 16:35:18 -0600

    Add function formatSpots to provide correct messaging depending on no. of spots left

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,19 @@ export default function DayListItem(props) {
     "day-list__item--full": !props.spots
   });
 
+  const formatSpots = (spots) => {
+    if(!spots) {
+      return "no spots remaining"
+    } 
+    if(spots === 1) {
+      return "1 spot remaining"
+    } else return `${spots} spots remaining`
+  }
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular" >{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
