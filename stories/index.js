e9df5536ad82f016e3c5fdad5d77b10bfcd956f2
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-21 05:51:03 -0600

    Add stories to test component InterviewerListItem in isolation

--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import DayListItem from "components/DayListItem";
+import InterviewerListItem from "components/InterviewerListItem";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import DayList  from "components/DayList";
@@ -67,3 +68,37 @@ storiesOf("Button", module)
     .add("Wednesday", () => (
         <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
     ));
+
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={action("setInterviewer")}
+    />
+  ));
