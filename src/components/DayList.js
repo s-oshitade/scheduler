8c514667729a7bc0ae2245a71c3276101a907e15
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-20 20:22:19 -0600

    Access DayList props and iterate to return an array of DayListItem components

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,10 +1,21 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayList () {
+export default function DayList({ days, day, setDay }) {
+  
+  const dayList = days.map(({ id, name, spots }) => (
+      <DayListItem
+      key={id}
+      name={name}
+      spots={spots} 
+      selected={name === day}
+      setDay={setDay}
+      />
+  ))
+
   return (
     <ul>
-
+      {dayList}
     </ul>
-  )
-};
+  );
+}
