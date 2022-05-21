42086ad32e2414b3e17a1a72eaa6d606edc36eae
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-20 21:46:34 -0600

    Add state to scheduler app

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList"
+
 
 const days = [
   {
@@ -22,6 +23,7 @@ const days = [
 ];
 
 export default function Application(props) {
+const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
@@ -34,8 +36,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={"Monday"}
-            setDay={day => console.log(day)}
+            day={day}
+            setDay={setDay}
           />
         </nav>
         <img
