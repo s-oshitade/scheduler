6a2d08c9a00ef0effdea6d0440f7b1e85a127cd2
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-22 14:32:54 -0600

    Implement component - Error and render on Storybook

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 
 import "index.scss";
@@ -150,14 +151,18 @@ storiesOf("Appointment", module)
 .add("Header", () => <Header time="12pm" />)
 .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
 .add("Show", () => <Show 
-                    student="Lydia Miller-Jones" 
-                    interviewer={interviewers[0]} 
-                    onEdit={action("onEdit")}
-                    onDelete={action("onDelete")}
-                    />)
+  student="Lydia Miller-Jones" 
+  interviewer={interviewers[0]} 
+  onEdit={action("onEdit")}
+  onDelete={action("onDelete")}
+/>)
 .add("Confirm", () => <Confirm
-message="Delete the appointment?" 
-onConfirm={action("onConfirm")}
-onCancel={action("onCancel")}
+  message="Delete the appointment?" 
+  onConfirm={action("onConfirm")}
+  onCancel={action("onCancel")}
 />)
 .add("Status", () => <Status message="Deleting..." />)
+.add("Error", () => <Error
+  message="Could not delete the appointment" 
+  onClose={action("onClose")}
+/>)
