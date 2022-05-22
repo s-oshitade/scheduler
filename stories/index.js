dfd2785ba5ced55d2fccf6f83cd19685a8368aeb
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-21 18:31:47 -0600

    Update the Clickable story of the InterviewListItem with an argument

--- stories/index.js
+++ stories/index.js
@@ -100,7 +100,7 @@ storiesOf("InterviewerListItem", module)
       id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      setInterviewer={action("setInterviewer")}
+      setInterviewer={() => action("setInterviewer")(interviewer.id)}
     />
   ));
 
