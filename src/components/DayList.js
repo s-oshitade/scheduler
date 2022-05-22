8c20c786f4194b88347ea5f31ca6cd08fcf0f758
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-21 19:20:40 -0600

    Change props.day to props.value and props.setDay to props.onChange

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,15 +1,15 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayList({ days, day, setDay }) {
+export default function DayList({ days, value, onChange }) {
   
   const dayList = days.map(({ id, name, spots }) => (
       <DayListItem
       key={id}
       name={name}
       spots={spots} 
-      selected={name === day}
-      setDay={setDay}
+      selected={name === value}
+      setDay={onChange}
       />
   ))
 
