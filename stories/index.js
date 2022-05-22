1fb3a4cebcf9d1a7b76d9e17977e5eb1e6155451
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-22 14:07:02 -0600

    Implement component - Confirm and render on Storybook

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show"
+import Confirm from "components/Appointment/Confirm"
 
 
 import "index.scss";
@@ -153,3 +154,8 @@ storiesOf("Appointment", module)
                     onEdit={action("onEdit")}
                     onDelete={action("onDelete")}
                     />)
+.add("Confirm", () => <Confirm
+message="Delete the appointment?" 
+onConfirm={action("onConfirm")}
+onCancel={action("onCancel")}
+/>)
