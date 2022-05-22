d1080c65f5ab1c2797462ce1d85342e714ade33d
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-22 17:32:06 -0600

    Add reset function

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,8 +1,17 @@
-import React from "react"
+import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form (props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null)
+
+  const reset = function (){
+    setStudent("");
+    setInterviewer(null);
+  }
+
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -12,15 +21,15 @@ export default function Form (props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-              This must be a controlled component
-              your code goes here
-            */
+            value={student}
+            onChange={(e) => setStudent(e.target.value)}
           />
         </form>
         <InterviewerList 
           interviewers={props.interviewers}
-        />
+          value={interviewer}
+          onChange={setInterviewer}
+        />s
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
