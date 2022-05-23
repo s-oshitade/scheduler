d7edaa9c6315979ebe43f2ef94dd2fee852badca
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-22 18:06:30 -0600

    Add save function

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -16,11 +16,15 @@ export default function Form (props) {
     props.onCancel();
   }
 
+  const save = function () {
+    props.onSave(student, interviewer);
+  }
+
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -34,12 +38,12 @@ export default function Form (props) {
           interviewers={props.interviewers}
           value={interviewer}
           onChange={setInterviewer}
-        />s
+        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button confirm onClick={save}>Save</Button>
         </section>
       </section>
     </main>
