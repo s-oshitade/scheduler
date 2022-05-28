6e429a3473e8d538c401268a8ff592672eb28996
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-28 15:27:47 -0600

    Add function getInterview

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -9,4 +9,19 @@ export function getAppointmentsForDay(state, day) {
     }
   }
   return appointmentsArr;
+};
+
+export function getInterview(state, interview) {
+  if(!interview){
+    return null;
+  }
+  const id = interview.interviewer;
+  return {
+    student:interview.student,
+    interviewer: {
+      id: id,
+      name: state.interviewers[id].name,
+      avatar: state.interviewers[id].avatar
+    }
+  }
 }
