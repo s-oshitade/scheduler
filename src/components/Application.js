7cd26a37bf2e1587d4451137e64f581d4d0c57a7
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-28 15:48:37 -0600

    Delete redundant code

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,58 +6,7 @@ import InterviewerList from "./InterviewerList";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay, getInterview } from "helpers/selectors.js"
 
-// const appointments = {
-//   "1": {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   "2": {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer:{
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   "3": {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   "4": {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer:{
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   "5": {
-//     id: 5,
-//     time: "4pm",
-//   }
-// };
-
-// const interviewers = [
-//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-// ];
-
 export default function Application(props) {
-// const [day, setDay] = useState("Monday");
-// const [days, setDays] = useState([]);
-// const [currentInterviewerID, setCurrentInterviewerID] = useState(null);
-
 //Combine the states into a single object
 const [state, setState] = useState({
   day: "Monday",
@@ -65,8 +14,7 @@ const [state, setState] = useState({
   appointments: {},
   interviewers: {}
 });
-// console.log("interviewers", state.interviewers);
-// let dailyAppointments = [];
+
 let dailyAppointments = getAppointmentsForDay(state, state.day)
 
 const appointmentsArray = dailyAppointments.map(appointment => {
@@ -77,7 +25,6 @@ const appointmentsArray = dailyAppointments.map(appointment => {
   })
 
   const setDay = day => setState({ ...state, day });
-  // const setDays = days => setState(prev => ({ ...prev, days }));
 
   const appointments = getAppointmentsForDay(state, state.day);
 
@@ -103,7 +50,6 @@ useEffect(() => {
     console.log("all", all)
     console.log(all[0].data);
     console.log(all[1].data);
-    // setDays([...all[0].data]);
     setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
  });
 }, []);
@@ -130,13 +76,6 @@ useEffect(() => {
           alt="Lighthouse Labs"
         />
       </section>
-      {/* <section className="schedule">
-        <InterviewerList
-          interviewers={interviewers}
-          interviewer = {currentInterviewerID}
-          setInterviewer = {setCurrentInterviewerID}
-        />
-      </section> */}
       <section className="schedule">
         {appointmentsArray}
         <Appointment key="last" time="5pm" />
