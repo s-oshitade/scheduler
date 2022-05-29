034249f23b69e08c71768a4b246bd3d21784ed26
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-29 05:16:44 -0600

    Conditionally render Empty and Show components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,12 +3,23 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment (props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
+    //conditionally render components
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview ? <Show student={ props.interview.student } interviewer={ props.interview.interviewer } /> : <Empty />}
+        {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+        {mode === SHOW && (<Show student={props.interview.student}
+                           interviewer={props.interview.interviewer}
+  />
+)}
     </article>
   )
 };
