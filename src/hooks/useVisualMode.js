553621f88071772cd9928defa84e7d0d9f3f2ce1
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-29 05:18:59 -0600

    Build custom hook - useVisualMode.js

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,23 @@ import { useState } from 'react';
 
 export default function useVisualMode (initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return {mode}
+  function transition (second, replace = false){
+    setMode(second);
+    if(replace) {
+      history.pop()
+    };
+    setHistory([...history, second]);
+  }
+
+  function back () {
+    if(history.length > 1){
+      history.pop();
+      const length = history.length;
+      setMode(history[length - 1]);
+      setHistory([...history])
+    }
+  };
+  return {mode, transition, back};
 }
