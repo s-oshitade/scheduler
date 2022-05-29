34efe017a4938ca23c84b8d3f78f687fec713524
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-29 06:14:20 -0600

    Update the appointment logic to handle transition to CREATE

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,9 +3,10 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 import useVisualMode from "hooks/useVisualMode";
 
-export default function Appointment (props) {
+export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
@@ -15,12 +16,20 @@ export default function Appointment (props) {
   return (
     //conditionally render components
     <article className="appointment">
-      <Header time={props.time}/>
-        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-        {mode === SHOW && (<Show student={props.interview.student}
-                           interviewer={props.interview.interviewer}
-  />
-)}
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          //Note: To be deleted: Set the interviewers prop to an empty array. We will implement this properly in a future activity.
+          interviewers={[]}
+        />
+      )}
     </article>
-  )
-};
+  );
+}
