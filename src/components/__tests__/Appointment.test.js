5ee703a46f211fd5aeab58f9e3cf687d33fbd663
Seun Oshitade <seun.oshitade@gmail.com>
2022-06-10 11:06:30 -0600

    Wrap tests in describe function

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -6,6 +6,16 @@ import Appointment from "components/Appointment";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Appointment />);
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+  
+  it("does something it is supposed to do", () => {
+    // ...
+  });
+
+  it("does something else it is supposed to do", () => {
+    // ...
+  });
 });
