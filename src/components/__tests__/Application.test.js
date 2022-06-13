f3b2d4d2514aa7a95a58415a35e80fb81212d39d
Seun Oshitade <seun.oshitade@gmail.com>
2022-06-13 07:50:38 -0600

    Refactor Application.test.js using async - await

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,16 +1,19 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent,
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+it("defaults to Monday and changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
-
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  });
+  await waitForElement(() => getByText("Monday"));
+  fireEvent.click(getByText("Tuesday"));
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
