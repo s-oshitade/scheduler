a9a334f60113ad07acd7b9aa59db93ddcb58fc84
Seun Oshitade <seun.oshitade@gmail.com>
2022-06-15 09:24:21 -0600

    Choose interviewer and click on Save button

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -6,5 +6,7 @@ describe("Appoitnments", () => {
     cy.contains("Monday");
     cy.get("[alt=Add]").first().click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    cy.get("[alt='Sylvia Palmer']").click();
+    cy.contains("Save").click();
   });
 });
